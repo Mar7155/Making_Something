74cca1ca7d325264db0354a755a3d22c00bcc5bf
mar7155 <mlm711105@gmail.com>
2025-03-30 01:13:41 -0600

    Add alias for src directory in Vite configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,12 @@ export default defineConfig({
 
   vite: {
     // @ts-ignore
+    resolve: {
+      alias: {
+        // @ts-ignore
+        "@": path.resolve("./src"), // Ensure this is correct
+      },
+    },
     plugins: [tailwindcss(), react()],
     optimizeDeps: {
       include: ["swiper"],
