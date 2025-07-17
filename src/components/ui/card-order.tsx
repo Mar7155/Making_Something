b81b0a6ce8f59d9f5dc9e8b9e3ab75b7315b1bb2
Mar7155 <mlm711105@gmail.com>
2025-07-16 19:35:00 -0600

    refactor: update import path for CardItem component to use absolute path

--- src/components/ui/card-order.tsx
+++ src/components/ui/card-order.tsx
@@ -1,7 +1,7 @@
 import { useStore } from '@nanostores/react'
 import { $cart, createOrder } from '@/lib/stores/cartStore'
 import { Card, CardAction, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import CardItem from './card-item'
+import CardItem from '@/components/ui/card-item'
 import { Button } from './button'
 import { useEffect, useState } from 'react'
 
