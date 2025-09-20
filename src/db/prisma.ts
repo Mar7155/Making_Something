0a00849e8bac932324c6e46a3de217e4f7d91d24
Mar7155 <mlm711105@gmail.com>
2025-09-20 05:26:49 -0600

    feat: initialize Prisma schema and generated configurating prisma

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../lib/generated/prisma'
+import { PrismaClient } from '../lib/generated/prisma'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
 const connectionString = `${process.env.DATABASE_URL}`
