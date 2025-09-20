25535911da8b38d90b945c0e67657be504e48e84
Mar7155 <mlm711105@gmail.com>
2025-09-20 06:28:04 -0600

    prsima config 12.1

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../lib/generated/prisma'
+import { PrismaClient } from '@prisma/client/edge'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
 const connectionString = `${process.env.DATABASE_URL}`
