5c6cdb569bda46b8dc228fa3f9e12bb4c8744d66
Mar7155 <mlm711105@gmail.com>
2025-09-20 05:13:29 -0600

    prisma configuration for cloudflare

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,10 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
-const connectionString = process.env.DATABASE_URL
-const pool = new PrismaNeon({ connectionString })
-
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-
-export const prisma = new PrismaClient({ adapter: pool });
+const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaNeon({ connectionString })
+export const prisma = new PrismaClient({ adapter })
 
