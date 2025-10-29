d48b039de3681512a5452572507cfe775d9d766e
Mar7155 <mlm711105@gmail.com>
2025-10-29 17:37:58 -0600

    simplify prisma config

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,24 +1,4 @@
-import 'dotenv/config'
 import { PrismaClient } from '@prisma/client/edge'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { neonConfig } from '@neondatabase/serverless'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-import ws from 'ws'
-neonConfig.webSocketConstructor = ws
-neonConfig.poolQueryViaFetch = true
-
-declare global {
-  var prisma: PrismaClient | undefined
-}
-
-const connectionString = process.env.DATABASE_URL!
-const adapter = new PrismaNeon({ connectionString })
-
-export const prisma =
-  globalThis.prisma ||
-  new PrismaClient({ adapter }).$extends(withAccelerate())
-
-if (process.env.NODE_ENV === 'development') globalThis.prisma = prisma as unknown as PrismaClient
-
-export default prisma
+export const prisma = new PrismaClient().$extends(withAccelerate())
